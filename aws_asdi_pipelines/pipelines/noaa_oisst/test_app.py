d634904b3fc015f9f1dc80e554f08a0ba9e315bc
Jamison French <jamisonjohnfrench@gmail.com>
2023-04-11 17:55:19 -0500

    lint from within tox env

--- aws_asdi_pipelines/pipelines/noaa_oisst/test_app.py
+++ aws_asdi_pipelines/pipelines/noaa_oisst/test_app.py
@@ -11,12 +11,12 @@ sns_message = {
     "MessageId": "e7beb21e-cc51-520e-b7fe-eeb480bf5670",
     "TopicArn": "arn:aws:sns:us-east-1:709902155096:NewCDRNDVIObject",
     "Subject": "Amazon S3 Notification",
-    "Message": "{\"Records\":[{\"eventVersion\":\"2.1\",\"eventSource\":\"aws:s3\",\"awsRegion\":\"us-east-1\",\"eventTime\":\"2023-04-07T16:54:05.487Z\",\"eventName\":\"ObjectCreated:Put\",\"userIdentity\":{\"principalId\":\"***\"},\"requestParameters\":{\"sourceIPAddress\":\"***\"},\"responseElements\":{\"x-amz-request-id\":\"***\",\"x-amz-id-2\":\"***\"},\"s3\":{\"s3SchemaVersion\":\"1.0\",\"configurationId\":\"7aa2472f-d0f4-46d2-a66b-a49db507bc56\",\"bucket\":{\"name\":\"noaa-cdr-sea-surface-temp-optimum-interpolation-pds\",\"ownerIdentity\":{\"principalId\":\"A1FTB9C2R32E6T\"},\"arn\":\"arn:aws:s3:::noaa-cdr-sea-surface-temp-optimum-interpolation-pds\"},\"object\":{\"key\":\"data/v2.1/avhrr/198109/oisst-avhrr-v02r01.19810901.nc\",\"size\":31608,\"eTag\":\"d79ac208ff860690b5ec71b28794652e\",\"sequencer\":\"0064304AAD6C368992\"}}}]}",
+    "Message": '{"Records":[{"eventVersion":"2.1","eventSource":"aws:s3","awsRegion":"us-east-1","eventTime":"2023-04-07T16:54:05.487Z","eventName":"ObjectCreated:Put","userIdentity":{"principalId":"***"},"requestParameters":{"sourceIPAddress":"***"},"responseElements":{"x-amz-request-id":"***","x-amz-id-2":"***"},"s3":{"s3SchemaVersion":"1.0","configurationId":"7aa2472f-d0f4-46d2-a66b-a49db507bc56","bucket":{"name":"noaa-cdr-sea-surface-temp-optimum-interpolation-pds","ownerIdentity":{"principalId":"A1FTB9C2R32E6T"},"arn":"arn:aws:s3:::noaa-cdr-sea-surface-temp-optimum-interpolation-pds"},"object":{"key":"data/v2.1/avhrr/198109/oisst-avhrr-v02r01.19810901.nc","size":31608,"eTag":"d79ac208ff860690b5ec71b28794652e","sequencer":"0064304AAD6C368992"}}}]}',
     "Timestamp": "2023-04-07T16:54:06.400Z",
     "SignatureVersion": "1",
     "Signature": "nK6FSyqZXRTxp8NW6eOXb0It05Gz2Y4ZRUHDUq+bTiSuC6YFnMsHeAywat2BkH4LNt/SENxcPkUAtsSn4KI2kRWLq0njBxDBAosW8z+JbE+qhzPnbRzsIXbLSHyN8ZRV0VTL1T55QH1fibNCpLe0tj6+Vm38qC57oYOYhiLLXJdPNKzKj1QVGDFx+Z+a8nKxACiQ94+DnlNQ+1Gj4+aHbTmdzxcUpeMjyL3t6y9Oiu3bMgR6XkKVHKx+nycrwVKpkPZZLxC8Wq1+3Z7UmKAkic5ooGURArycGEatcqCRzzovowHd5+l31nQFRrrDGtt7j2yIXm9AcJskHSfFBxYb9Q==",
     "SigningCertURL": "https://sns.us-east-1.amazonaws.com/SimpleNotificationService-56e67fcb41f6fec09b0196692625d385.pem",
-    "UnsubscribeURL": "https://sns.us-east-1.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-east-1:709902155096:NewCDRNDVIObject:63acafcc-4ff3-4b64-9cc4-c559c23391b8"
+    "UnsubscribeURL": "https://sns.us-east-1.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-east-1:709902155096:NewCDRNDVIObject:63acafcc-4ff3-4b64-9cc4-c559c23391b8",
 }
 body = json.dumps(sns_message)
 
