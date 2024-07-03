3dadfa1af27f9d5b8ffb4361ac16b380b29fd1d9
Jamison French <jamison@developmentseed.org>
2024-07-03 14:49:08 -0500

    chore: update to model_validators

--- stactools_pipelines/models/pipeline.py
+++ stactools_pipelines/models/pipeline.py
@@ -1,7 +1,8 @@
 from enum import Enum
 from typing import Optional
+from typing_extensions import Self
 
-from pydantic import BaseModel, root_validator
+from pydantic import BaseModel, model_validator
 
 
 class ComputeEnum(str, Enum):
@@ -13,22 +14,22 @@ class Pipeline(BaseModel):
     compute: ComputeEnum
     secret_arn: str
     ingestor_url: str
-    sns: Optional[str]
-    inventory_location: Optional[str]
-    historic_frequency: Optional[int]
-    initial_chunk: Optional[str]
+    sns: Optional[str] = None
+    inventory_location: Optional[str] = None
+    historic_frequency: Optional[int] = None
+    initial_chunk: Optional[str] = None
 
-    @root_validator(pre=False)
-    def historic_frequency_if_inventory_location(cls, values):
-        if values["inventory_location"] and values["historic_frequency"] is None:
+    @model_validator(mode="after")
+    def historic_frequency_if_inventory_location(self) -> Self:
+        if self.inventory_location and self.historic_frequency is None:
             raise ValueError("Must include historic_frequency with inventory_location")
-        return values
+        return self
 
-    @root_validator(pre=False)
-    def initial_chunk_if_historic_frequency_greater_than_0(cls, values):
-        if values["historic_frequency"]:
-            if values["historic_frequency"] > 0 and values["initial_chunk"] is None:
+    @model_validator(mode="after")
+    def initial_chunk_if_historic_frequency_greater_than_0(self) -> Self:
+        if self.historic_frequency:
+            if self.historic_frequency > 0 and self.initial_chunk is None:
                 raise ValueError(
                     "Must include initial_chunk when historic_frequency > 0"
                 )
-        return values
+        return self
